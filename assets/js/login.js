4236d98c2bbd0b721cf29f25e4112c7c60f4ce3b
itYuZe <498339706@qq.com>
2022-04-17 18:00:40 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -37,11 +37,11 @@ $(function () {
         let data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }
         $.post('/api/reguser',
             data, function (res) {
-            if (res.status !== 0) { return layer.msg(res.message) }
-            layer.msg('注册成功,请登录')
-            //模拟人的点击行为
-            $('#link_login').click()
-        })
+                if (res.status !== 0) { return layer.msg(res.message) }
+                layer.msg('注册成功,请登录')
+                //模拟人的点击行为
+                $('#link_login').click()
+            })
     })
     //监听登录表单的提交事件
     $('#form_login').submit(function (e) {
@@ -58,7 +58,7 @@ $(function () {
                 }
                 layer.msg('登录成功')
                 //将登录成功得到的 token 字符串,保存到localStorage中
-                localStorage.setItem('token',res.token)
+                localStorage.setItem('token', res.token)
                 //跳转到后台主页
                 location.href = '/index.html'
                 // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzUzMCwidXNlcm5hbWUiOiJhZHMiLCJwYXNzd29yZCI6IiIsIm5pY2tuYW1lIjoiIiwiZW1haWwiOiIiLCJ1c2VyX3BpYyI6IiIsImlhdCI6MTY1MDExNjc5MiwiZXhwIjoxNjUwMTUyNzkyfQ.LseK8soI7T-EIeX_SPWMEpcUr5xTvSGvsUyMpsaUV6U
